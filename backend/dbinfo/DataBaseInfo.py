4e77126a9f822b986867d833224be8e416f7cf3e
xiongli <946115360@qq.com>
2025-06-17 22:59:09 +0800

    修改数据库密码的获取方式

--- backend/dbinfo/DataBaseInfo.py
+++ backend/dbinfo/DataBaseInfo.py
@@ -1,8 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy import text
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URI = 'mysql+pymysql://root:945942@localhost:3308/ai_chat'
+password = os.getenv('MYSQL_PASSWORD')
+DATABASE_URI = f'mysql+pymysql://root:{password}@127.0.0.1:3308/ai_chat'
 # engine = create_engine(DATABASE_URI, pool_recycle=300, echo=True)  # 使用心跳回收机制, 打开sql日志记录
 engine = create_engine(DATABASE_URI, pool_recycle=300)  # 使用心跳回收机制
 Session = sessionmaker(bind=engine)
